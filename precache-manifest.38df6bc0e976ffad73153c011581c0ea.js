bf39d26142fe10bd54fd8a54b6facfe8a9b1eeee
Nikki <archiektor@gmail.com>
2020-06-27 09:07:53 +0200

    Updates

---
+++ precache-manifest.38df6bc0e976ffad73153c011581c0ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cb430131486a85291b8a24d22be24c",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "f97fdbd78a302ce282cf",
+    "url": "/TodoList/static/css/main.e2737c13.chunk.css"
+  },
+  {
+    "revision": "1f0d04ba091beea42f04",
+    "url": "/TodoList/static/js/2.68392e0b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.68392e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f97fdbd78a302ce282cf",
+    "url": "/TodoList/static/js/main.8a484b0f.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
